1eef8b0cbe2a9b80b0c5ec264fe0eede4a8b6bcd
Craig Buchanan <ctbuchan1@gmail.com>
2023-02-17 22:31:24 -0500

    format result with display

--- groovy_ssl/src/md5.rs
+++ groovy_ssl/src/md5.rs
@@ -1,27 +1,59 @@
 use std::cmp;
-use std::fmt::Write;
+use std::fmt;
 
 const A0: u32 = 0x67452301;
 const B0: u32 = 0xefcdab89;
 const C0: u32 = 0x98badcfe;
 const D0: u32 = 0x10325476;
 
+#[cfg_attr(rustfmt, rustfmt::skip)]
 const S: [u32; 64] = [
-    7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9,
-    14, 20, 5, 9, 14, 20, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 6, 10, 15,
-    21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21,
+     7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,
+     5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,
+     4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,
+     6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,
 ];
 
+#[cfg_attr(rustfmt, rustfmt::skip)]
 const K: [u32; 64] = [
-    0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
-    0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
-    0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
-    0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
-    0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
-    0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
-    0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
-    0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391,
+    0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+    0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+    0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+    0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+    0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+    0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+    0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
+    0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+    0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+    0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+    0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
+    0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+    0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
+    0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+    0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+    0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391,
 ];
+
+#[derive(Clone, Copy)]
+pub struct MD5Result([u8; 16]);
+
+impl IntoIterator for MD5Result {
+    type Item = u8;
+    type IntoIter = std::array::IntoIter<Self::Item, 16>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
+impl fmt::Display for MD5Result {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for byte in self.into_iter() {
+            write!(f, "{:02x}", byte)?;
+        }
+        Ok(())
+    }
+}
 
 fn md5_round(message: [u32; 16], a0: u32, b0: u32, c0: u32, d0: u32) -> (u32, u32, u32, u32) {
     let (mut a, mut b, mut c, mut d) = (a0, b0, c0, d0);
@@ -44,7 +76,7 @@ fn md5_round(message: [u32; 16], a0: u32, b0: u32, c0: u32, d0: u32) -> (u32, u3
     (a, b, c, d)
 }
 
-pub fn md5(message: &[u8]) -> [u8; 16] {
+pub fn md5(message: &[u8]) -> MD5Result {
     let message_length = message.len();
     // 1 byte end + 8 byte length
     let chunks_count = ((message_length + 1 + 8) / 64) + 1;
@@ -97,7 +129,7 @@ pub fn md5(message: &[u8]) -> [u8; 16] {
         );
     }
 
-    let ret: [u8; 16] = [
+    MD5Result([
         (a & 0xFF).try_into().unwrap(),
         ((a >> 8) & 0xFF).try_into().unwrap(),
         ((a >> 16) & 0xFF).try_into().unwrap(),
@@ -114,16 +146,7 @@ pub fn md5(message: &[u8]) -> [u8; 16] {
         ((d >> 8) & 0xFF).try_into().unwrap(),
         ((d >> 16) & 0xFF).try_into().unwrap(),
         ((d >> 24) & 0xFF).try_into().unwrap(),
-    ];
-    ret
-}
-
-pub fn to_string(hash: &[u8; 16]) -> String {
-    let mut s = String::with_capacity(hash.len() * 2);
-    for &byte in hash {
-        write!(&mut s, "{:02x}", byte).unwrap();
-    }
-    s
+    ])
 }
 
 #[cfg(test)]
@@ -133,7 +156,7 @@ mod tests {
     #[test]
     fn empty_string() {
         assert_eq!(
-            to_string(&md5(b"")),
+            format!("{}", md5(b"")),
             String::from("d41d8cd98f00b204e9800998ecf8427e")
         );
     }
@@ -141,7 +164,7 @@ mod tests {
     #[test]
     fn string1() {
         assert_eq!(
-            to_string(&md5(b"a")),
+            format!("{}", md5(b"a")),
             String::from("0cc175b9c0f1b6a831c399e269772661")
         );
     }
@@ -149,7 +172,7 @@ mod tests {
     #[test]
     fn string2() {
         assert_eq!(
-            to_string(&md5(b"abc")),
+            format!("{}", md5(b"abc")),
             String::from("900150983cd24fb0d6963f7d28e17f72")
         );
     }
@@ -157,7 +180,7 @@ mod tests {
     #[test]
     fn string3() {
         assert_eq!(
-            to_string(&md5(b"message digest")),
+            format!("{}", md5(b"message digest")),
             String::from("f96b697d7cb7938d525a2f31aaf161d0")
         );
     }
@@ -165,7 +188,7 @@ mod tests {
     #[test]
     fn string4() {
         assert_eq!(
-            to_string(&md5(b"abcdefghijklmnopqrstuvwxyz")),
+            format!("{}", md5(b"abcdefghijklmnopqrstuvwxyz")),
             String::from("c3fcd3d76192e4007dfb496cca67e13b")
         );
     }
@@ -173,9 +196,10 @@ mod tests {
     #[test]
     fn string5() {
         assert_eq!(
-            to_string(&md5(
-                b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-            )),
+            format!(
+                "{}",
+                md5(b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
+            ),
             String::from("d174ab98d277d9f5a5611c2c9f419d9f")
         );
     }
@@ -183,9 +207,7 @@ mod tests {
     #[test]
     fn string6() {
         assert_eq!(
-            to_string(&md5(
-                b"12345678901234567890123456789012345678901234567890123456789012345678901234567890"
-            )),
+            format!("{}", md5(b"12345678901234567890123456789012345678901234567890123456789012345678901234567890")),
             String::from("57edf4a22be3c955ac49da2e2107b67a")
         );
     }
